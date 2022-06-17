2dbe30379fcee936ae503bbd5f1ee8dd322376d4
Aditya Mulik <aditya.mulik@gmail.com>
2022-06-16 22:58:00 -0400

    Update pre_translation.sh

--- pre_translation.sh
+++ pre_translation.sh
@@ -1,18 +1,10 @@
 # Change Directory to clones
 cd _clones/
 
-# Build Hub Environment
-cp .compose.env.example .compose.env
-echo $"ANSIBLE_HUB_UI_PATH='_clones/'" >> .compose.env
-./compose build
-
 # Extract UI Strings
-./compose run --rm --user=0 \
--v _translations:/translations \
-ui \
-sh -c "npm run gettext:extract && cp /hub/app/locale/en.po /translations/en.po"
+npm run gettext:extract
 
 # Move files to translations folder
-# mv pinakes/locale/en/LC_MESSAGES/django.po translations/
+mv locale/en.po translations/
 
 sleep 5
